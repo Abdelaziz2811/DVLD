c4523012b7477952eaaf919495ebf0852ae55d3a
Abdelaziz <Abdobm2811@gmail.com>
2025-06-13 16:07:26 +0100

    Implement UC_TestDetails

---
+++ DVLD/User_Controls/Applications Section/Tests/UC_TestDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.User_Controls.Applications_Section.Tests
+{
+    public partial class UC_TestDetails : UserControl
+    {
+        public UC_TestDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
