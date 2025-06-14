25ff3904fe5b6f421eb136c0384fae94f9ced6fa
Abdelaziz <Abdobm2811@gmail.com>
2025-06-14 15:09:18 +0100

    Implement Tests Class - DVLD_DAL

---
+++ DVLD_DAL/Applications/TestAppointments/clsTests_DAL.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DAL.Applications.Tests
+{
+    public static class clsTests_DAL
+    {
+        public static DataTable LoadTests()
+        {
+            SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
+
+            string query = @"SELECT * FROM Tests";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            DataTable DTTestAppointments = new DataTable();
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader Reader = command.ExecuteReader();
+
+                if (Reader.HasRows)
+                    DTTestAppointments.Load(Reader);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return DTTestAppointments;
+        }
+
+        public static bool Find(int TestID, ref int TestAppointmentID, ref bool TestResult, ref string Notes, ref int CreatedByUserID)
+        {
+            SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
+
+            string query = @"SELECT * FROM Tests
+                                 WHERE TestID = @TestID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestID", TestID);
+
+            bool IsFound = false;
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader Reader = command.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    IsFound = true;
+                    TestAppointmentID = (int)Reader["TestAppointmentID"];
+                    TestResult = (bool)Reader["TestResult"];
+                    Notes = Reader["Notes"] != DBNull.Value ? Reader["Notes"].ToString() : null;
+                    CreatedByUserID = (int)Reader["CreatedByUserID"];
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return IsFound;
+        }
+
+        public static int Add(int TestAppointmentID, bool TestResult, string Notes, int CreatedByUserID)
+        {
+            SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
+
+            string query = @"INSERT INTO Tests
+                             VALUES(                                   
+                                   @TestAppointmentID,
+                                   @TestResult,
+                                   @Notes,
+                                   @CreatedByUserID);
+                             SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            command.Parameters.AddWithValue("@TestResult", TestResult);
+            command.Parameters.AddWithValue("@Notes", Notes);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+
+            int TestID = 0;
+
+            try
+            {
+                connection.Open();
+
+                object Result = command.ExecuteScalar();
+
+                if (Result != null && int.TryParse(Result.ToString(), out int ID))
+                    TestID = ID;
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return TestID;
+        }
+
+        public static bool Update(int TestID, int TestAppointmentID, bool TestResult, string Notes, int CreatedByUserID)
+        {
+            SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
+
+            string query = @"UPDATE Tests
+                             SET                                   
+                                   TestAppointmentID = @TestAppointmentID,
+                                   TestResult = @TestResult,
+                                   Notes = @Notes,
+                                   CreatedByUserID = @CreatedByUserID
+                             WHERE TestID = @TestID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestID", TestID);
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            command.Parameters.AddWithValue("@TestResult", TestResult);
+            command.Parameters.AddWithValue("@Notes", Notes);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+
+            int RowsAffected = 0;
+
+            try
+            {
+                connection.Open();
+
+                RowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return RowsAffected > 0;
+        }
+
+        public static bool IsPass(int TestAppointmentID)
+        {
+            SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
+
+            string query = @"SELECT TestResult FROM Tests
+                             WHERE TestAppointmentID = @TestAppointmentID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            
+            bool Pass = false;
+
+            try
+            {
+                connection.Open();
+
+                object TestResult = command.ExecuteScalar();
+
+                if (TestResult != null && int.TryParse(TestResult.ToString(), out int Result))
+                {
+                    if (Result == 1) Pass = true;
+                    else Pass = false;
+                }
+                    
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return Pass;
+        }
+    }
+}
