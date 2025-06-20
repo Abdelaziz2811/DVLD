b995301bbacf7e80b2b7ef8613be90c503d1837d
Abdelaziz <Abdobm2811@gmail.com>
2025-06-20 18:01:01 +0100

    clsLicenseClasses_BLL

--- DVLD_BLL/License Classes/clsLicenseClasses_BLL.cs
+++ DVLD_BLL/License Classes/clsLicenseClasses_BLL.cs
@@ -9,14 +9,49 @@ using System.Threading.Tasks;
 
 namespace DVLD_BLL.License_Classes
 {
-    public static class clsLicenseClasses_BLL
+    public class clsLicenseClasses_BLL
     {
+        public int LicenseClassID {  get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public byte MinimumAllowedAge { get; set; }
+        public byte DefaultValidityLength { get; set; }
+        public decimal ClassFees { get; set; }
+
+        private clsLicenseClasses_BLL(int LicenseClassID, string ClassName, string ClassDescription,
+                                      byte MinimumAllowedAge, byte DefaultValidityLength, decimal ClassFees)
+        {
+            this.LicenseClassID = LicenseClassID;
+            this.ClassName = ClassName;
+            this.ClassDescription = ClassDescription;
+            this.MinimumAllowedAge = MinimumAllowedAge;
+            this.DefaultValidityLength = DefaultValidityLength;
+            this.ClassFees = ClassFees;
+        }
+
         public static DataTable GetLicenseClasses()
         {
             return clsLicenseClasses_DAL.GetLicenseClasses();
         }
 
-        public static int LicenseClassID(string ClassName)
+        public static clsLicenseClasses_BLL Find(string ClassName)
+        {
+            int LicenseClassID = 0;
+            string ClassDescription = string.Empty;
+            byte MinimumAllowedAge = 0;
+            byte DefaultValidityLength = 0;
+            decimal ClassFees = 0;
+
+            if (clsLicenseClasses_DAL.Find(ref LicenseClassID, ClassName, ref ClassDescription, ref MinimumAllowedAge,
+                                           ref DefaultValidityLength, ref ClassFees))
+            {
+                return new clsLicenseClasses_BLL(LicenseClassID, ClassName, ClassDescription,MinimumAllowedAge,
+                                                 DefaultValidityLength, ClassFees);
+            }
+            return null;
+        }
+
+        public static int GetLicenseClassID(string ClassName)
         {
             return clsLicenseClasses_DAL.LicenseClassID(ClassName);
         }
