5e20eba134b293fde38b5306843823ad61b73c03
Abdelaziz <Abdobm2811@gmail.com>
2025-06-21 22:00:10 +0100

    Update LicenseInfo.cs

--- DVLD/User_Controls/Applications Section/Local License/UC_LicenseInfo.cs
+++ DVLD/User_Controls/Applications Section/Local License/UC_LicenseInfo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DVLD.Sections.Applications.Manage_Applications.Local_License_Applications;
+using DVLD_BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +21,27 @@ namespace DVLD.User_Controls.Applications_Section.Local_License
 
         private void BTN_PersonDetails_Click(object sender, EventArgs e)
         {
+            if (LB_NationalNo.Text != "----")
+            {
+                clsPerson_BLL Person = clsPerson_BLL.Find(LB_NationalNo.Text);
+                if (Person != null)
+                {
+                    PersonDetails personDetails = new PersonDetails(ref Person);
+                    personDetails.LoadPersonInfo += LoadPersonInfo;
+                    personDetails.ShowDialog();
+                }
+            }
+        }
 
+        void LoadPersonInfo(clsPerson_BLL Person)
+        {
+            clsPerson_BLL person = clsPerson_BLL.Find(Person.PersonID);
+            LB_Name.Text = person.FirstName + ' ' + person.SecondName + ' ' + person.ThirdName + ' ' + person.LastName;
+            LB_NationalNo.Text = person.NationalNo;
+            LB_Gender.Text = person.Gender.ToString();
+            LB_BirthDate.Text = person.BirthDate.ToString("d");
+            if (person.ImagePath != null)
+                PB_PersonImage.Image = Image.FromFile(person.ImagePath);
         }
     }
 }
