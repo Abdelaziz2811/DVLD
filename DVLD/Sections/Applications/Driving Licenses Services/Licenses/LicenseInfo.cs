312a28174a9ceece884f0f0935a16bb868eac1ed
Abdelaziz <Abdobm2811@gmail.com>
2025-06-26 21:21:15 +0100

    Add LicenseInfo Form

---
+++ DVLD/Sections/Applications/Driving Licenses Services/Licenses/LicenseInfo.cs
@@ -0,0 +1,36 @@
+﻿using DVLD_BLL;
+using DVLD_BLL.Applications.Licenses;
+using DVLD_BLL.Applications.LocalLicenseApplication;
+using DVLD_BLL.License_Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Sections.Applications.Driving_Licenses_Services.New_Driving_License.Local_License
+{
+    public partial class LicenseInfo : Form
+    {
+        clsLicense_BLL License;
+        clsPerson_BLL Person;
+        clsLocalLicenseApplication_BLL LocalLicenseApplication;
+        public LicenseInfo(clsLocalLicenseApplication_BLL LocalLicenseApplication)
+        {
+            InitializeComponent();
+
+            this.LocalLicenseApplication = LocalLicenseApplication;
+            License = clsLicense_BLL.FindByAppID(LocalLicenseApplication.ApplicationID);
+            Person = clsPerson_BLL.Find(LocalLicenseApplication.NationalNo);
+        }
+
+        private void LicenseInfo_Load(object sender, EventArgs e)
+        {
+            UC_LicenseInfo.LoadLicenseInfo(License, Person, LocalLicenseApplication);
+        }
+    }
+}
