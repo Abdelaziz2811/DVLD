5cf03c022d955bf48bae956898bf84c69176bd9c
Abdelaziz <Abdobm2811@gmail.com>
2025-06-30 18:13:45 +0100

    Add User Control InternationalLicenseInfo.Designer.cs

---
+++ DVLD/User_Controls/Applications Section/International Licenses/UC_InternationalLicenseInfo.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace DVLD.User_Controls.Applications_Section.International_Licenses
+{
+    partial class UC_InternationalLicenseInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GB_LicenseInfo = new System.Windows.Forms.GroupBox();
+            this.LB_IssueDate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LB_ApplicationID = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.LB_ExpirationDate = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.LB_DriverID = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.LB_Active = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.LB_ILicenseID = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.LB_BirthDate = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.LB_Gender = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.LB_NationalNo = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LB_LLicenseID = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LB_Name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PB_PersonImage = new System.Windows.Forms.PictureBox();
+            this.GB_LicenseInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_PersonImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GB_LicenseInfo
+            // 
+            this.GB_LicenseInfo.BackColor = System.Drawing.SystemColors.Window;
+            this.GB_LicenseInfo.Controls.Add(this.LB_IssueDate);
+            this.GB_LicenseInfo.Controls.Add(this.label3);
+            this.GB_LicenseInfo.Controls.Add(this.LB_ApplicationID);
+            this.GB_LicenseInfo.Controls.Add(this.label11);
+            this.GB_LicenseInfo.Controls.Add(this.LB_ExpirationDate);
+            this.GB_LicenseInfo.Controls.Add(this.label15);
+            this.GB_LicenseInfo.Controls.Add(this.LB_DriverID);
+            this.GB_LicenseInfo.Controls.Add(this.label14);
+            this.GB_LicenseInfo.Controls.Add(this.LB_Active);
+            this.GB_LicenseInfo.Controls.Add(this.label16);
+            this.GB_LicenseInfo.Controls.Add(this.LB_ILicenseID);
+            this.GB_LicenseInfo.Controls.Add(this.label8);
+            this.GB_LicenseInfo.Controls.Add(this.LB_BirthDate);
+            this.GB_LicenseInfo.Controls.Add(this.label10);
+            this.GB_LicenseInfo.Controls.Add(this.LB_Gender);
+            this.GB_LicenseInfo.Controls.Add(this.label12);
+            this.GB_LicenseInfo.Controls.Add(this.LB_NationalNo);
+            this.GB_LicenseInfo.Controls.Add(this.label6);
+            this.GB_LicenseInfo.Controls.Add(this.LB_LLicenseID);
+            this.GB_LicenseInfo.Controls.Add(this.label4);
+            this.GB_LicenseInfo.Controls.Add(this.LB_Name);
+            this.GB_LicenseInfo.Controls.Add(this.label1);
+            this.GB_LicenseInfo.Controls.Add(this.PB_PersonImage);
+            this.GB_LicenseInfo.Font = new System.Drawing.Font("Tahoma", 9.75F);
+            this.GB_LicenseInfo.Location = new System.Drawing.Point(3, 3);
+            this.GB_LicenseInfo.Name = "GB_LicenseInfo";
+            this.GB_LicenseInfo.Size = new System.Drawing.Size(775, 315);
+            this.GB_LicenseInfo.TabIndex = 51;
+            this.GB_LicenseInfo.TabStop = false;
+            this.GB_LicenseInfo.Text = "License Info";
+            // 
+            // LB_IssueDate
+            // 
+            this.LB_IssueDate.AutoSize = true;
+            this.LB_IssueDate.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_IssueDate.Location = new System.Drawing.Point(195, 168);
+            this.LB_IssueDate.Name = "LB_IssueDate";
+            this.LB_IssueDate.Size = new System.Drawing.Size(33, 19);
+            this.LB_IssueDate.TabIndex = 77;
+            this.LB_IssueDate.Text = "----";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 19);
+            this.label3.TabIndex = 76;
+            this.label3.Text = "Issue Date :";
+            // 
+            // LB_ApplicationID
+            // 
+            this.LB_ApplicationID.AutoSize = true;
+            this.LB_ApplicationID.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_ApplicationID.Location = new System.Drawing.Point(508, 90);
+            this.LB_ApplicationID.Name = "LB_ApplicationID";
+            this.LB_ApplicationID.Size = new System.Drawing.Size(33, 19);
+            this.LB_ApplicationID.TabIndex = 73;
+            this.LB_ApplicationID.Text = "----";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(354, 90);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(136, 19);
+            this.label11.TabIndex = 72;
+            this.label11.Text = "Application ID :";
+            // 
+            // LB_ExpirationDate
+            // 
+            this.LB_ExpirationDate.AutoSize = true;
+            this.LB_ExpirationDate.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_ExpirationDate.Location = new System.Drawing.Point(195, 207);
+            this.LB_ExpirationDate.Name = "LB_ExpirationDate";
+            this.LB_ExpirationDate.Size = new System.Drawing.Size(33, 19);
+            this.LB_ExpirationDate.TabIndex = 71;
+            this.LB_ExpirationDate.Text = "----";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(41, 207);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(148, 19);
+            this.label15.TabIndex = 70;
+            this.label15.Text = "Expiration Date :";
+            // 
+            // LB_DriverID
+            // 
+            this.LB_DriverID.AutoSize = true;
+            this.LB_DriverID.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_DriverID.Location = new System.Drawing.Point(476, 129);
+            this.LB_DriverID.Name = "LB_DriverID";
+            this.LB_DriverID.Size = new System.Drawing.Size(33, 19);
+            this.LB_DriverID.TabIndex = 68;
+            this.LB_DriverID.Text = "----";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(354, 129);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(94, 19);
+            this.label14.TabIndex = 67;
+            this.label14.Text = "Driver ID :";
+            // 
+            // LB_Active
+            // 
+            this.LB_Active.AutoSize = true;
+            this.LB_Active.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Active.Location = new System.Drawing.Point(195, 246);
+            this.LB_Active.Name = "LB_Active";
+            this.LB_Active.Size = new System.Drawing.Size(33, 19);
+            this.LB_Active.TabIndex = 66;
+            this.LB_Active.Text = "----";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(41, 246);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(71, 19);
+            this.label16.TabIndex = 65;
+            this.label16.Text = "Active :";
+            // 
+            // LB_ILicenseID
+            // 
+            this.LB_ILicenseID.AutoSize = true;
+            this.LB_ILicenseID.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_ILicenseID.Location = new System.Drawing.Point(195, 90);
+            this.LB_ILicenseID.Name = "LB_ILicenseID";
+            this.LB_ILicenseID.Size = new System.Drawing.Size(33, 19);
+            this.LB_ILicenseID.TabIndex = 62;
+            this.LB_ILicenseID.Text = "----";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(41, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 19);
+            this.label8.TabIndex = 61;
+            this.label8.Text = "I.License ID :";
+            // 
+            // LB_BirthDate
+            // 
+            this.LB_BirthDate.AutoSize = true;
+            this.LB_BirthDate.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_BirthDate.Location = new System.Drawing.Point(476, 246);
+            this.LB_BirthDate.Name = "LB_BirthDate";
+            this.LB_BirthDate.Size = new System.Drawing.Size(33, 19);
+            this.LB_BirthDate.TabIndex = 60;
+            this.LB_BirthDate.Text = "----";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(354, 207);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 19);
+            this.label10.TabIndex = 59;
+            this.label10.Text = "Gender :";
+            // 
+            // LB_Gender
+            // 
+            this.LB_Gender.AutoSize = true;
+            this.LB_Gender.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Gender.Location = new System.Drawing.Point(476, 207);
+            this.LB_Gender.Name = "LB_Gender";
+            this.LB_Gender.Size = new System.Drawing.Size(33, 19);
+            this.LB_Gender.TabIndex = 58;
+            this.LB_Gender.Text = "----";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(354, 168);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(116, 19);
+            this.label12.TabIndex = 57;
+            this.label12.Text = "National No :";
+            // 
+            // LB_NationalNo
+            // 
+            this.LB_NationalNo.AutoSize = true;
+            this.LB_NationalNo.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_NationalNo.Location = new System.Drawing.Point(476, 168);
+            this.LB_NationalNo.Name = "LB_NationalNo";
+            this.LB_NationalNo.Size = new System.Drawing.Size(33, 19);
+            this.LB_NationalNo.TabIndex = 56;
+            this.LB_NationalNo.Text = "----";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(354, 246);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 19);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "Birth Date :";
+            // 
+            // LB_LLicenseID
+            // 
+            this.LB_LLicenseID.AutoSize = true;
+            this.LB_LLicenseID.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_LLicenseID.Location = new System.Drawing.Point(195, 129);
+            this.LB_LLicenseID.Name = "LB_LLicenseID";
+            this.LB_LLicenseID.Size = new System.Drawing.Size(33, 19);
+            this.LB_LLicenseID.TabIndex = 54;
+            this.LB_LLicenseID.Text = "----";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 19);
+            this.label4.TabIndex = 53;
+            this.label4.Text = "L.License ID :";
+            // 
+            // LB_Name
+            // 
+            this.LB_Name.AutoSize = true;
+            this.LB_Name.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Name.Location = new System.Drawing.Point(195, 51);
+            this.LB_Name.Name = "LB_Name";
+            this.LB_Name.Size = new System.Drawing.Size(33, 19);
+            this.LB_Name.TabIndex = 52;
+            this.LB_Name.Text = "----";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 19);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Name :";
+            // 
+            // PB_PersonImage
+            // 
+            this.PB_PersonImage.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.PB_PersonImage.Location = new System.Drawing.Point(601, 98);
+            this.PB_PersonImage.Name = "PB_PersonImage";
+            this.PB_PersonImage.Size = new System.Drawing.Size(132, 116);
+            this.PB_PersonImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PB_PersonImage.TabIndex = 50;
+            this.PB_PersonImage.TabStop = false;
+            // 
+            // UC_InternationalLicenseInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.GB_LicenseInfo);
+            this.Name = "UC_InternationalLicenseInfo";
+            this.Size = new System.Drawing.Size(781, 321);
+            this.GB_LicenseInfo.ResumeLayout(false);
+            this.GB_LicenseInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_PersonImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GB_LicenseInfo;
+        public System.Windows.Forms.Label LB_IssueDate;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label LB_ApplicationID;
+        private System.Windows.Forms.Label label11;
+        public System.Windows.Forms.Label LB_ExpirationDate;
+        private System.Windows.Forms.Label label15;
+        public System.Windows.Forms.Label LB_DriverID;
+        private System.Windows.Forms.Label label14;
+        public System.Windows.Forms.Label LB_Active;
+        private System.Windows.Forms.Label label16;
+        public System.Windows.Forms.Label LB_ILicenseID;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.Label LB_BirthDate;
+        private System.Windows.Forms.Label label10;
+        public System.Windows.Forms.Label LB_Gender;
+        private System.Windows.Forms.Label label12;
+        public System.Windows.Forms.Label LB_NationalNo;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label LB_LLicenseID;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label LB_Name;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.PictureBox PB_PersonImage;
+    }
+}
