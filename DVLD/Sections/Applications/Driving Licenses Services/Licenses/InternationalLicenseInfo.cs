04ec7530e8771e114561117cf5796bb93909546b
Abdelaziz <Abdobm2811@gmail.com>
2025-06-30 18:03:34 +0100

    Add internationalLicenseInfo.cs

---
+++ DVLD/Sections/Applications/Driving Licenses Services/Licenses/InternationalLicenseInfo.cs
@@ -0,0 +1,33 @@
+﻿using DVLD_BLL;
+using DVLD_BLL.Applications.Licenses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Sections.Applications.Driving_Licenses_Services.Licenses
+{  
+    public partial class InternationalLicenseInfo : Form
+    {
+        clsInternationalLicense_BLL InternationalLicense;
+        clsPerson_BLL Person;
+
+        public InternationalLicenseInfo(clsInternationalLicense_BLL InternationalLicense, clsPerson_BLL Person)
+        {
+            InitializeComponent();
+
+            this.InternationalLicense = InternationalLicense;
+            this.Person = Person;
+        }
+
+        private void InternationalLicenseInfo_Load(object sender, EventArgs e)
+        {
+            UC_InternationalLicenseInfo.LoadInternationalLicenseInfo(InternationalLicense, Person);
+        }
+    }
+}
