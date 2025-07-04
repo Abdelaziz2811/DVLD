b3e32031d86dde2a39efaf35fc9a6022ed5df1b4
Abdelaziz <Abdobm2811@gmail.com>
2025-07-04 17:53:16 +0100

    Update DetainedLicenses_DAL

--- DVLD_DAL/Applications/Licenses/clsDetainedLicenses_DAL.cs
+++ DVLD_DAL/Applications/Licenses/clsDetainedLicenses_DAL.cs
@@ -11,6 +11,130 @@ namespace DVLD_DAL.Applications.Licenses
 {
     public static class clsDetainedLicenses_DAL
     {
+        public static int Add(int LicenseID, DateTime DetainDate, decimal FineFees, int CreatedByUserID,
+            bool IsReleased, DateTime ReleaseDate, int ReleasedByUserID, int ReleaseApplicationID)
+        {
+            SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
+
+            string query = @"INSERT INTO DetainedLicenses
+                             VALUES
+                                   (@LicenseID
+                                   ,@DetainDate
+                                   ,@FineFees
+                                   ,@CreatedByUserID
+                                   ,@IsReleased
+                                   ,@ReleaseDate
+                                   ,@ReleasedByUserID
+                                   ,@ReleaseApplicationID);
+                             SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@LicenseID", LicenseID);
+            command.Parameters.AddWithValue("@DetainDate", DetainDate);
+            command.Parameters.AddWithValue("@FineFees", FineFees);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+            command.Parameters.AddWithValue("@IsReleased", IsReleased);
+
+            if (ReleaseDate == DateTime.MinValue)
+            command.Parameters.AddWithValue("@ReleaseDate", DBNull.Value);
+            else
+                command.Parameters.AddWithValue("@ReleaseDate", ReleaseDate);
+
+            if (ReleasedByUserID == 0)
+            command.Parameters.AddWithValue("@ReleasedByUserID", DBNull.Value);
+            else
+                command.Parameters.AddWithValue("@ReleasedByUserID", ReleasedByUserID);
+
+            if (ReleaseApplicationID == 0)
+            command.Parameters.AddWithValue("@ReleaseApplicationID", DBNull.Value);
+            else
+                command.Parameters.AddWithValue("@ReleaseApplicationID", ReleaseApplicationID);
+
+            int DetainID = 0;
+
+            try
+            {
+                connection.Open();
+
+                object Result = command.ExecuteScalar();
+
+                if (Result != null && int.TryParse(Result.ToString(), out int ID))
+                    DetainID = ID;
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return DetainID;
+        }
+
+        public static bool Update(int DetainID, int LicenseID, DateTime DetainDate, decimal FineFees, int CreatedByUserID,
+                                  bool IsReleased, DateTime ReleaseDate, int ReleasedByUserID, int ReleaseApplicationID)
+        {
+            SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
+
+            string query = @"UPDATE Licenses
+                             SET                                   
+                                    LicenseID = @LicenseID,
+                                    DetainDate = @DetainDate,
+                                    FineFees = @FineFees,
+                                    CreatedByUserID = @CreatedByUserID,
+                                    IsReleased = @IsReleased,
+                                    ReleaseDate = @ReleaseDate,
+                                    ReleasedByUserID = @ReleasedByUserID,
+                                    ReleaseApplicationID = @ReleaseApplicationID
+                             WHERE DetainID = @DetainID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@DetainID", DetainID);
+            command.Parameters.AddWithValue("@LicenseID", LicenseID);
+            command.Parameters.AddWithValue("@DetainDate", DetainDate);
+            command.Parameters.AddWithValue("@FineFees", FineFees);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+            command.Parameters.AddWithValue("@IsReleased", IsReleased);
+
+            if (ReleaseDate == DateTime.MinValue)
+                command.Parameters.AddWithValue("@ReleaseDate", DBNull.Value);
+            else
+                command.Parameters.AddWithValue("@ReleaseDate", ReleaseDate);
+
+            if (ReleasedByUserID == 0)
+                command.Parameters.AddWithValue("@ReleasedByUserID", DBNull.Value);
+            else
+                command.Parameters.AddWithValue("@ReleasedByUserID", ReleasedByUserID);
+
+            if (ReleaseApplicationID == 0)
+                command.Parameters.AddWithValue("@ReleaseApplicationID", DBNull.Value);
+            else
+                command.Parameters.AddWithValue("@ReleaseApplicationID", ReleaseApplicationID);
+
+            int RowsAffected = 0;
+
+            try
+            {
+                connection.Open();
+
+                RowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return RowsAffected > 0;
+        }
+
         public static bool IsExists(int LicenseID)
         {
             SqlConnection connection = new SqlConnection(DAL_Settings.ConnectionString);
