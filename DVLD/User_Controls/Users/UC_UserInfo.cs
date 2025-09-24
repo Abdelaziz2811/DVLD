bd62cc429ebbf84af663252e9aa26c6661ad2eed
Abdelaziz <Abdobm2811@gmail.com>
2025-09-24 09:55:56 +0100

    Update UC_UserInfo

--- DVLD/User_Controls/Users/UC_UserInfo.cs
+++ DVLD/User_Controls/Users/UC_UserInfo.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DVLD_BLL;
+using DVLD_BLL.Users;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,21 @@ namespace DVLD.User_Controls.Users_Section_Controls
         public UC_UserInfo()
         {
             InitializeComponent();
+        }
+
+        public void GetUserInfo(clsUser_BLL User)
+        {
+            clsPerson_BLL Person = clsPerson_BLL.Find(User.PersonID);
+
+            if (Person != null)
+                Person_Info.GetPersonDetails(Person);
+
+            LB_UserID.Text = User.UserID.ToString();
+            LB_UserName.Text = User.UserName.ToString();
+
+            if (User.IsActive)
+                LB_IsActive.Text = "Yes";
+            else LB_IsActive.Text = "No";
         }
     }
 }
